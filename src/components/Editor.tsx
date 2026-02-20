bbb21033171e5b0d30d2d193b444e51abfcd35d1
Bruna <72671246+BrunaDev@users.noreply.github.com>
2026-02-20 09:08:30 -0300

    Update lowlight import in Editor component
    Replace createLowlight with lowlight import from common.

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,7 +2,7 @@ import { useEditor, EditorContent, BubbleMenu, FloatingMenu } from "@tiptap/reac
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import StarterKit from "@tiptap/starter-kit";
 import { initialContent } from "./initialContent";
-import { createLowlight } from 'lowlight';
+import { lowlight } from 'lowlight/lib/common';
 import js from 'highlight.js/lib/languages/javascript';
 
 import { RxFontBold, RxFontItalic, RxStrikethrough, RxCode, RxChevronDown, RxChatBubble } from "react-icons/rx";
@@ -10,8 +10,6 @@ import { RxFontBold, RxFontItalic, RxStrikethrough, RxCode, RxChevronDown, RxCha
 import 'highlight.js/styles/tokyo-night-dark.css';
 import { BubbleButton } from "./BubbleButton";
 import { FloatButton } from "./FloatButton";
-
-const lowlight = createLowlight();
 
 lowlight.registerLanguage('javascript', js);
 
